1d7b4c9ca7e2c087677df5c0f51bbbf820c777d0
David Bruant <bruant.d@gmail.com>
2019-07-10 16:19:34 +0200

    Affichage d'une formule basée sur Svelte

--- scripts/entry/formula-examples.js
+++ scripts/entry/formula-examples.js
@@ -8,72 +8,112 @@ import makeLigneBudgetFilterFromFormula from '../DocumentBudgetaireQueryLanguage
 import {fromXMLDocument} from '../finance/planDeCompte.js'
 
 
-function makeTable(rows, year, planDeCompte) {
-    return Bouture.section([
-        Bouture.h1('CA Gironde ', year),
-        Bouture.h2(rows.length, ' elements | ', sum(rows.map(r => r['MtReal'])).toFixed(2) + '€'),
-        Bouture.table([
-            Bouture.thead.tr(['RD', 'FI', 'Fonction', 'Nature', 'Montant'].map(t => Bouture.th(t))),
-            Bouture.tbody(
-                rows.map(r => {
-                    return Bouture.tr([
-                        Bouture.td(r['CodRD']),
-                        Bouture.td(planDeCompte.ligneBudgetFI(r)),
-                        Bouture.td(r['Fonction']),
-                        Bouture.td(r['Nature']),
-                        Bouture.td(r['MtReal'].toFixed(2) + '€')
-                    ])
-                })
-            )
-        ])
-    ])
-}
-
 const planDeCompteP = xml('./data/plansDeCompte/plan-de-compte-M52-M52-2017.xml')
 .then(fromXMLDocument)
 
-const docBudgP = xml('./data/CA/CA2017BPAL.xml')
+const docBudgP = xml('./data/CA/gironde/CA2017BPAL.xml')
 .then(xmlDocumentToDocumentBudgetaire)
 .catch(console.error)
+
+const examples = [
+    {
+        formula: 'D',
+        description: 'toutes les dépenses'
+    },
+    {
+        formula: 'R',
+        description: 'toutes les recettes'
+    },
+    {
+        formula: 'RI',
+        description: `toutes les recettes d'investissement`
+    },
+    {
+        formula: 'DF',
+        description: 'toutes les dépenses de fonctionnement'
+    },
+    {
+        formula: 'DF∩F4',
+        description: 'toutes les dépenses de fonctionnement de la fonction 4'
+    },
+    {
+        formula: 'DF∩(F4 ∪ F5)',
+        description: 'toutes les dépenses de fonctionnement des fonctions 4 et 5'
+    },
+    {
+        formula: 'DF∩(C64111 ∪ C6451)',
+        description: 'toutes les dépenses de fonctionnement des natures 64111 et 6451 (salaire + URSSAF)'
+    },
+    {
+        formula: 'DF∩(C64111 ∪ C6451)∩F52',
+        description: 'toutes les dépenses de fonctionnement salaires+URSSAF lié à la fonction 52 (Personnes handicapées)'
+    },
+    {
+        formula: 'DF∩((F50∩(C64121 ∪ C64126)) ∪ (F51 - C6526))',
+        description: 'Gironde - social - enfance'
+    },
+    {
+        formula: 'RI∩Ch16',
+        description: `toutes les recettes d'investissement du chapitre 16 (emprunts)`
+    },
+    {
+        formula: 'RF∩(C7478141 ∪ C7478142 ∪ C74788∩F53∩Ann2016)',
+        description: `Gironde - recettes de fonctionnement - Conférence des financeurs`
+    },
+    {
+        formula: 'DF∩C60 - (F4∪F5∪F8∪F621)',
+        description: `Gironde - dépense de fonctionnement - Achats et fournitures`
+    }
+];
+
+const getFormulaLignes = memoize((formula, docBudg, planDeCompte) => {
+    return [...docBudg['rows']].filter(makeLigneBudgetFilterFromFormula(formula, planDeCompte))
+})
 
 docBudgP
 .then(docBudg => console.log('docBudg', docBudg))
 
+
+const formula = decodeURIComponent(location.hash.slice(1))
+
+const state = {
+    formula,
+    planDeCompte: undefined,
+    lignes: undefined,
+    examples
+}
+
 document.addEventListener('DOMContentLoaded', e => {
-    const input = document.body.querySelector('input');
+
+    let changeHashTimeout;
+
+    const formulaExamplesUI = new FormulaExamples({
+        target: document.body.querySelector('main'),
+        props: state
+    });
 
     Promise.all([ docBudgP, planDeCompteP ])
     .then(([docBudg, planDeCompte]) => {
 
-        function makeOutputFromFormula(formula, planDeCompte) {
-            const filter = makeLigneBudgetFilterFromFormula(formula, planDeCompte)
+        state.lignes = getFormulaLignes(formula, docBudg, planDeCompte)
+        state.planDeCompte = planDeCompte
 
-            return Bouture.output(
-                [ makeTable(docBudg['rows'].filter(filter), docBudg['Exer'], planDeCompte) ]
-            ).getElement()
-        }
+        formulaExamplesUI.$set(state)
 
-        const memzMakeOutputFromFormula = memoize(makeOutputFromFormula)
-
-        let changeHashTimeout;
-
-        input.addEventListener('input', e => {
-            const formula = e.target.value.trim();
-
-            document.body.querySelector('output').replaceWith(
-                memzMakeOutputFromFormula(formula, planDeCompte)
-            )
+        formulaExamplesUI.$on('formula-change', formula => {
+            state.lignes = getFormulaLignes(formula, docBudg, planDeCompte)
 
             // save in hash if formula stayed unchanged for 3secs
             clearTimeout(changeHashTimeout)
             changeHashTimeout = setTimeout(() => {
                 location.hash = encodeURIComponent(formula)
+                changeHashTimeout = undefined;
             }, 3000)
-
         })
+        
     })
 
-    function hashUpdate() {
+    /*function hashUpdate() {
         const hashValue = decodeURIComponent(location.hash.slice(1));
 
         if (hashValue && hashValue !== input.value) {
@@ -84,81 +124,13 @@ document.addEventListener('DOMContentLoaded', e => {
 
     // empty input (sometimes filled with pre-refresh value)
     input.value = "";
-    input.focus()
+    input.focus()*/
 
     // initialize input vith hash value
-    Promise.all([ docBudgP, planDeCompteP ]).then(hashUpdate)
+    /*Promise.all([ docBudgP, planDeCompteP ]).then(hashUpdate)
 
     window.addEventListener('hashchange', hashUpdate)
-
-
-    // Create examples list
-    const examples = [
-        {
-            formula: 'D',
-            description: 'toutes les dépenses'
-        },
-        {
-            formula: 'R',
-            description: 'toutes les recettes'
-        },
-        {
-            formula: 'RI',
-            description: `toutes les recettes d'investissement`
-        },
-        {
-            formula: 'DF',
-            description: 'toutes les dépenses de fonctionnement'
-        },
-        {
-            formula: 'DF∩F4',
-            description: 'toutes les dépenses de fonctionnement de la fonction 4'
-        },
-        {
-            formula: 'DF∩(F4 ∪ F5)',
-            description: 'toutes les dépenses de fonctionnement des fonctions 4 et 5'
-        },
-        {
-            formula: 'DF∩(C64111 ∪ C6451)',
-            description: 'toutes les dépenses de fonctionnement des natures 64111 et 6451 (salaire + URSSAF)'
-        },
-        {
-            formula: 'DF∩(C64111 ∪ C6451)∩F52',
-            description: 'toutes les dépenses de fonctionnement salaires+URSSAF lié à la fonction 52 (Personnes handicapées)'
-        },
-        {
-            formula: 'DF∩((F50∩(C64121 ∪ C64126)) ∪ (F51 - C6526))',
-            description: 'Gironde - social - enfance'
-        },
-        {
-            formula: 'RI∩Ch16',
-            description: `toutes les recettes d'investissement du chapitre 16 (emprunts)`
-        },
-        {
-            formula: 'RF∩(C7478141 ∪ C7478142 ∪ C74788∩F53∩Ann2016)',
-            description: `Gironde - recettes de fonctionnement - Conférence des financeurs`
-        },
-        {
-            formula: 'DF∩C60 - (F4∪F5∪F8∪F621)',
-            description: `Gironde - dépense de fonctionnement - Achats et fournitures`
-        }
-    ];
-
-    /*const ul = Bouture.ul(examples.map(({ formula, description }) => {
-        return Bouture.li([
-            Bouture.a({ href: `#${encodeURIComponent(formula)}` }).code(formula),
-            Bouture.span(` : ${description}`)
-        ])
-    })).getElement();
-
-    document.body.querySelector('ul.examples').replaceWith(ul)*/
-
-    const fe = new FormulaExamples({
-        target: document.body.querySelector('main'),
-        /*props: {
-            name: 'world'
-        }*/
-    });
+    */
 
 
 }, { once: true })
--- scripts/formula-examples/FormulaExamples.svelte
+++ scripts/formula-examples/FormulaExamples.svelte
@@ -1,11 +1,38 @@
 <script>
-	export let name;
+	import FormulaPreview from './FormulaPreview.svelte'
+
+	export let formula;
+	export let planDeCompte;
+	export let lignes;
+	export let examples;
 </script>
 
 <style>
-	h2 {
-		color: purple;
-	}
+input.formula{
+	width: 100%;
+	margin-bottom: 0.5em;
+
+	font-size: 1.6em;
+	font-family: monospace;
+}
 </style>
 
-<h2>Exemples de ya formules</h2>
+<h1>Outil de test de formules</h1>
+
+<h2>Un outil pour filtrer les lignes d'un document budgétaires</h2>
+<p>Pour le moment, cet outil ne filtre que les données des <a href="https://www.datalocale.fr/dataset/comptes-administratifs-budget-principal-donnees-budgetaires-du-departement-de-la-gironde">Comptes Administratifs du Département de la Gironde</a> pour l'année 2017</p>
+
+<h2>Exemples de yopo formules</h2>
+
+<ul class="examples">
+	{#each examples as {formula, description}}
+		<li>
+			<a href="#{encodeURIComponent(formula)}"><code>{formula}</code></a>
+			<span> : {description}</span>
+		</li>
+	{/each}
+</ul>
+
+<input class="formula" type="text" bind:value={formula}>
+
+<FormulaPreview {lignes} {planDeCompte}/>
